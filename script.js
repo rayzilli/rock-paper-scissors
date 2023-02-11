1831628b64f4b93275ccf6e8511f3a7426404009
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-11 00:56:37 -0500

    Program works in console

--- script.js
+++ script.js
@@ -14,24 +14,32 @@ function playRound (playerSelection, computerSelection){
    if (playerSelection === computerSelection){
      return "tie " + playerSelection + " " + computerSelection;
    }
-   else if (playerSelection === "rock" && computerSelection === "scissors"){
+   else if (playerSelection  === "rock" && computerSelection === "scissors"){
      return "player wins "  + playerSelection + " " + computerSelection;
    }
-   else if (playerSelection === "paper" && computerSelection === "rock"){
+   else if (playerSelection  === "paper" && computerSelection === "rock"){
      return "player wins " + playerSelection + " " + computerSelection; 
    }
-   else if (playerSelection === "scissors"  && computerSelection === "paper"){
+   else if (playerSelection  === "scissors"  && computerSelection === "paper"){
      return "player wins " + playerSelection + " " + computerSelection;
    }
    else return "computer wins "  + computerSelection + " " + playerSelection ; 
   
 
 }
+function game(){
+    for (let i = 0; i<5; i++){
+       const playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+       const computerSelection = getComputerChoice();
+       playRound()
+       console.log(playRound (playerSelection, computerSelection));
+    }
+}
 
-const playerSelection = "paper";
-const computerSelection = getComputerChoice();
 
-console.log(playRound (playerSelection, computerSelection));
+
+game(); 
+
 
 
 
